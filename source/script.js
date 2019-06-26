166db8b8197ca548e35d3c8842e98341ff677187
MichorovTech <michorov@outlook.com>
2019-06-26 10:54:34 +0300

    Set base multipliers

--- source/script.js
+++ source/script.js
@@ -1,25 +1,37 @@
 let player = {
-    balance: 0
+    balance: 0,
+    baseOnClickBalance: 0.3,
+    BasePerSecBalance: 0.3
 };
 
 let frameTime = 1000/30;
 
 window.onload = function(){
+    initiateClickableArea();
+
     updateBalance();
     setupClickableArea();
 
-    gameLoop();
+    window.requestAnimationFrame(gameLoop);
+}
+
+function initiateClickableArea(){
+    let clickArea = document.getElementsByClassName("clickArea")[0];
+    let bgImage = document.createElement('img');
+    bgImage.setAttribute("src","img/background.jpg");
+    bgImage.classList.add("clickImage");
+    clickArea.appendChild(bgImage);
 }
 
 function updateBalance(){
     var balance = document.getElementsByClassName('balanceText')[0];
-    balance.textContent = player.balance;
+    balance.textContent = player.balance.toFixed(2);
 }
 
 function setupClickableArea(){
-    let area = document.getElementsByClassName('clickArea')[0];
+    let area = document.getElementsByClassName('clickImage')[0];
     area.addEventListener("click", function(){
-        player.balance+=1;
+        player.balance+=player.onClickBalance;
         updateBalance();
     })
     
@@ -27,9 +39,9 @@ function setupClickableArea(){
 
 gameLoop = () => {
     setTimeout(() => {
-        player.balance+=1;
+        player.balance+=player.perSecBalance/100;
         updateBalance();
         
         window.requestAnimationFrame(gameLoop);
-    }, 1000);
+    }, 10);
 }
