d038f9055e1800579aebdf8eb11e790b8e0fd040
MichorovTech <michorov@outlook.com>
2019-06-26 13:21:44 +0300

    Implemented followers

--- source/script.js
+++ source/script.js
@@ -1,18 +1,66 @@
 let player = {
     balance: 0,
-    baseOnClickBalance: 0.01,
-    BasePerSecBalance: 0.01
+    baseOnClickBalance: 1,
+    BasePerSecBalance: 0.01,
+    perSecBalance: 0,
+    onClickBalance: 0,
+    followers: []
 };
 
 let frameTime = 1000/30;
 
+
 window.onload = function(){
     initiateClickableArea();
+    setupPlayerFollowers();
+
+    setFollowers();
 
     updateBalance();
     setupClickableArea();
 
     window.requestAnimationFrame(gameLoop);
+}
+
+function setupPlayerFollowers(){
+    for(let i=0; i<10; i++){
+        let current = {
+            id: i+1,
+            level: 0,
+            name: `Follower ${i+1}`,
+            upgradePrice: function() {return (this.level+1)*10},
+            multiplier: function() { if(this.level === 0){
+                return 1;
+            }else{
+                return 1+ this.level/10}
+            }
+        }
+
+        player.followers.push(current);
+    }
+}
+
+function setFollowers(){
+    let leftNav = document.getElementsByClassName("left-sidenav")[0];
+    
+    player.followers.forEach((element) => {
+        let followerContainer = document.createElement("div");
+        let followerInfo = document.createElement("div");
+        followerInfo.textContent = `${element.name}        Price: ${element.upgradePrice()}`;
+        followerContainer.appendChild(followerInfo);
+        let upgradeButton = document.createElement("span");
+        upgradeButton.textContent = "Upgrade";
+    
+        upgradeButton.addEventListener("click", function(){
+            if(player.balance >= element.upgradePrice()){
+                player.balance -= element.upgradePrice();
+                element.level+=1;
+            }
+        })
+    
+        followerContainer.appendChild(upgradeButton);
+        leftNav.appendChild(followerContainer);
+    })
 }
 
 function initiateClickableArea(){
@@ -37,10 +85,25 @@ function setupClickableArea(){
     
 }
 
+function calculatePerSecBalance(){
+    let totalMultiplier = 1;
+    player.followers.forEach(element => {
+        totalMultiplier = totalMultiplier * element.multiplier();
+    });
+    console.log(totalMultiplier);
+    player.perSecBalance = player.BasePerSecBalance * totalMultiplier
+}
+
+function updateFollowers(){
+
+}
+
 gameLoop = () => {
     setTimeout(() => {
-        player.balance+=player.BasePerSecBalance/100;
+        calculatePerSecBalance();
+        player.balance+=player.perSecBalance/100;
         updateBalance();
+        updateFollowers();
         
         window.requestAnimationFrame(gameLoop);
     }, 10);
